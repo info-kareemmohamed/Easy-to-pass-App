9e014bbd5589769d01103a33ab0883efa5625732
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-06-26 19:32:49 +0300

    Add  : Constant Class

---
+++ app/src/main/java/com/example/easytopass/util/Constant.kt
@@ -0,0 +1,10 @@
+package com.example.easytopass.util
+
+class Constant {
+    companion object {
+        const val INTENT_CHAPTER_LIST: String = "INTENT_CHAPTER_LIST"
+        const val INTENT_QUESTION_LIST: String = "INTENT_QUESTION_LIST"
+
+
+    }
+}
