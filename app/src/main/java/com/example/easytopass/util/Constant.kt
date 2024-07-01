d7c08264327b93b72af380c91593690db67c6625
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-07-01 14:07:43 +0300

    add constant intent

--- app/src/main/java/com/example/easytopass/util/Constant.kt
+++ app/src/main/java/com/example/easytopass/util/Constant.kt
@@ -4,6 +4,9 @@ class Constant {
     companion object {
         const val INTENT_CHAPTER_LIST: String = "INTENT_CHAPTER_LIST"
         const val INTENT_QUESTION_LIST: String = "INTENT_QUESTION_LIST"
+        const val INTENT_ReportQUESTION_RIGHTANSWER: String = "INTENT_ReportQUESTION_RIGHTANSWER"
+        const val INTENT_ReportQUESTION_WRONGANSWER: String = "INTENT_ReportQUESTION_WRONGANSWER"
+        const val INTENT_ReportQUESTION_TOTALQUESTIONS: String = "INTENT_ReportQUESTION_TOTALQUESTIONS"
 
 
     }
